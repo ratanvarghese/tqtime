0ea5f86b20036acd3b97db6f30592a84fc741444
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2018-08-14 09:53:55 -0400

    Add tqdate tool

---
+++ _example/tqdate.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/ratanvarghese/tqtime"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	inputFormat := flag.String("inputformat", time.UnixDate, "Reference date in Gregorian input format")
+	input := flag.String("input", "", "Gregorian input date, use stdin if omitted")
+	help := flag.Bool("help", false, "Print command-line options")
+	flag.Parse()
+
+	if *help {
+		flag.PrintDefaults()
+		return
+	}
+
+	var inputReader *bufio.Reader
+	if *input == "" {
+		inputReader = bufio.NewReader(os.Stdin)
+	} else {
+		inputReader = bufio.NewReader(strings.NewReader((*input) + "\n"))
+	}
+
+	for {
+		line, lineErr := inputReader.ReadString('\n')
+		if lineErr != nil {
+			break
+		}
+		t, parseErr := time.Parse(*inputFormat, strings.TrimSpace(line))
+		if parseErr != nil {
+			log.Fatal(parseErr.Error())
+			break
+		}
+		long := tqtime.LongDate(t.Year(), t.YearDay())
+		fmt.Println(long)
+	}
+}
